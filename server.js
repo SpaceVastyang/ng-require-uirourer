f8169147615c84806db584cf26927a6ebb83b138
yangyuhao <136585913@qq.com>
2017-03-04 14:44:18 +0800

    updae

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // var http = require('http');
+// var server = http.createServer(app);
+
 var express = require('express');
 var path = require('path');
 var app = express();
-// var server = http.createServer(app);
+
 app.use(express.static(path.join(__dirname, 'ngrequire')));
 app.listen(3001,function(){
 	console.log("server start in 3001" )
